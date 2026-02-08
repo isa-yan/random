841baf0259d006f1d6421c1973ab1510605e6f6e
Isa Yan <isabel.yyj.yan@gmail.com>
2026-02-08 04:03:53 -0500

    delete

--- quiz.js
+++ quiz.js
@@ -26,25 +26,10 @@ gays.onclick = function(){
     
 }
 
+/* ok so um yeah this is the more actual part like avoiding*/
+const speed = 10
 
-
-
-/* 2/2/2026 so i'm going to bed soon but um yeah mental note to add something
-to put the buttons under the picture and add something on top the dancing queen so yeah :D */
-
-/*function areugay(admitit){
-    admitit = prompt("are you gay")
-    if (admitit!=""){
-        alert("Honey, you clicked on this quiz for a reason. You're probably gay and that's ok :D")
-    }else{
-        alert("WHY DID YOU CLICK ON THIS QUIZ JUST TO LEAVE IT EMPTYHOLY FUCK")
-    }
-
-
+let escapeVector = {
+    x:0,
+    y:0
 }
-
-
-
-areugay();
-
-*/
