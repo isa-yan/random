d42d5b8614ed6e6bfa8604d2515a1a37552dd3f5
Isa Yan <isabel.yyj.yan@gmail.com>
2026-02-15 03:27:52 -0500

    did some stuff, some changes, better js

--- quiz.js
+++ quiz.js
@@ -27,9 +27,21 @@ gays.onclick = function(){
 }
 
 /* ok so um yeah this is the more actual part like avoiding*/
-const speed = 10
+var avoiding = document.getElementById('donttouchme');
 
-let escapeVector = {
-    x:0,
-    y:0
+/* logic 
+1. generate a random number of both x and y
+2. make the top and the left of the element these x and y's
+3. on click, the elemnt moves in any direction away from the mouse 
+*/
+
+avoiding.addEventListener("click",() => {runaway();})
+
+
+function runaway(){
+    const newTop = Math.round((Math.random()) * 100);
+    const newLeft = Math.round((Math.random()) * 100); 
+    avoiding.style.top = newTop + "px"
+    avoiding.style.left = newLeft + "px"
+    console.log('ran!')
 }
